4a121af731426c24e96ff1eaf9039b51822195fc
novariann89-yn <novariann89@gmail.com>
2026-03-12 23:52:49 +0700

    ah elah sudah bner ngatur git sm github. disini aku belajar user input keseluruhan, dari Bro Code. belum paham, besok gas lagi

---
+++ latihan3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>  
+
+int main(){
+
+  int kelas;
+  printf("kamu kelas berapa?\n");
+  scanf("%d", &kelas);
+  printf("kamu kelas %d\n", kelas);
+
+
+
+  // bikin yang laen
+  // yang aku belum jago or paham.
+
+  char huruf;
+
+  printf("masukkan satu huruf apapun\n");
+  huruf = getchar();
+  printf("huruf kamu: %c\n", huruf);
+
+
+
+  return 0;
+}
