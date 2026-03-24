3919f250c9f1f4a1b2a463d9e3ec14412f830597
novariann89-yn <novariann89@gmail.com>
2026-03-24 23:39:05 +0700

    latihan bikin program pengkonversi satuan berat kgs dan lbs

--- latihan4.c
+++ latihan4.c
@@ -1,66 +1,31 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
 
-  float pangkat=pow(8.0, 2.0);
+  int choice=0;
+  float kgs=0.0f;
+  float lbs=0.0f;
 
-  float a=3.5;
-  float b=8.3;
-  float c=12.6;
-  float d =30;
-  float e=-35;
-  float f=-2.5;
-  float g=6.7;
+  printf("weight converter program\n");
+  printf("1. kg to lbs\n");
+  printf("2. lbs to kgs\n");
+  printf("choose between both (1 or 2): ");
+  scanf("%d", choice);
 
-  printf("8 pangkat 3 = %.2f\n", pow(b, a));
-  printf("30 pangkat 8 = %.2f\n", pow(d, b));
-  printf("30 pangkat 3 = %.2f\n", pow(d, a));
-  printf("12 pangkat 3 = %.2f\n", pow(c, a));
-  printf("12 pangkat 8 = %.2f\n", pow(c, b));
-  printf("^^^ pow atau perpangkatan ^^^\n \n");
+  if (choice == 1){
+    printf("input weight in kgs\n");
+    scanf("%f", &kgs);
+    lbs=kgs*2.20462;
+    printf("%.2f kgs is equal to %.2f lbs\n", kgs, lbs);
+  }
+  else if (choice == 2){
+    printf("input weight in kgs\n");
+    scanf("%f", &lbs);
+    kgs=lbs/2.20462;
+    printf("%.2f lbs is equal to %.2f kgs\n", lbs, kgs);
+  }
+  else {
+    printf("invalid input! please choose either 1 or 2!!\n");
+  }
 
-  printf("8 kuadrat = %.2f\n", sqrt(b));
-  printf("12 kuadrat = %.2f\n", sqrt(c));
-  printf("3 kuadrat = %.2f\n", sqrt(a));
-  printf("30 kuadrat = %.2f\n", sqrt(d));
-  printf("^^^ sqrt atau kuadrat ^^^\n \n");
-
-  printf("jika 6.7 dibulatkan maka jadi %.2f\n", fabs(g));
-  printf("jika -2.5 dibulatkan maka jadi %.2f\n", fabs(f));
-  printf("jika -35 dibulatkan maka jadi %.2f\n", fabs(e));
-  printf("^^^ fabs atau nilai mutlak untuk desimal ^^^\n \n"); 
-
-  printf("pembulatan keatas dari 3.5 = %.2f\n", ceil(a));
-  printf("pembulatan keatas dari 8.3 = %.2f\n", ceil(b));
-  printf("pembulatan keatas dari 12.6 = %.2f\n", ceil(c));
-  printf("^^^ ceil atau pembulatan keatas ^^^\n \n");
-
-  printf("pembulatan kebawah dari 3.5 = %.2f\n", ceil(a));
-  printf("pembulatan kebawah dari 8.3 = %.2f\n", ceil(b));
-  printf("pembulatan kebawah dari 12.6 = %.2f\n", ceil(c));
-  printf("^^^ floor atau pembulatan kebawah ^^^\n \n");
-
-  printf("pembulatan ke yang terdekat dari 3.5 = %.2f\n", floor(a));
-  printf("pembulatan ke yang terdekat dari 8.3 = %.2f\n", floor(a));
-  printf("pembulatan ke yang terdekat dari 12.6 = %.2f\n", floor(a));
-  printf("^^^ round atau pembulatan ke yang terdekat ^^^\n \n");
-
-  printf("logaritma natural dari %.2f = %.2f\n", a, log(a));
-  printf("logaritma natural dari %.2f = %.2f\n", b, log(b));
-  printf("logaritma natural dari %.2f = %.2f\n", c, log(c));
-  printf("^^^ log atau logaritma natural ^^^\n \n");
-
-  printf("log10 dari %.2f = %.2f\n", a, log10(a));
-  printf("log10 dari %.2f = %.2f\n", b, log10(b));
-  printf("log10 dari %.2f = %.2f\n", c, log10(c));
-  printf("^^^ log10 atau logaritma basis 10\n \n");
-  
-
-
-  float yo=70;
-  printf("%.2f jika di kuadrat akan menjadi %.2f\n", yo, sqrt(yo));
-  
-
-  return 0;
 }
