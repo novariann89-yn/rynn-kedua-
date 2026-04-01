01891167bdd857e611abea4e5c72255caf94f755
novariann89-yn <novariann89@gmail.com>
2026-04-01 21:16:11 +0700

    train myself to remake this program

--- latihan1.c
+++ latihan1.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
-#include <string.h>
-
-// membuat program pemesanan barang
 
 int main(){
 
-  char barang[20];
-  float harga;
-  int jumlah;
+  char choice;
+  float kgs=0.0f;
+  float lbs=0.0f;
 
-  printf("~~~ Welcome To Our Shop!! ~~~\n");
+  printf("--- weight conversion program ---\n");
+  printf("1. ubah dari KGs ke LBs\n");
+  printf("2. ubah dari LBs ke KGs\n");
+  printf("pilih (1 or 2) = ");
+  scanf("%c", &choice);
 
-  printf("\nmasukkan nama barang: ");
-  fgets(barang, sizeof(barang), stdin);
-
-  printf("masukkan harga per barang nya: ");
-  scanf("%g", &harga);
-
-  printf("masukkan jumlah barang: ");
-  scanf("%d", &jumlah);
-  printf("\n");
-
-  (void)getchar();
-  printf("barang nya %s", barang);
-  printf("harga per satu barang nya %g\n", harga);
-  printf("jumlah barang nya %d\n", jumlah);
-
-  float total = harga*jumlah;
-  printf("\njadi totalnya %.3f rupiah\n", total);
+  if (choice == '1'){
+    printf("masukkan beban dalam KGs = ");
+    scanf("%f", &kgs);
+    lbs=kgs*2.20462;
+    printf("hasil nya = %.2f KGs sama dengan %.2f LBs\n", kgs, lbs);
+  }
+  else if (choice == '2'){
+    printf("masukkan beban dalam LBs = ");
+    scanf("%f", &lbs);
+    kgs=lbs/2.20462;
+    printf("hasil nya = %.2f LBs sama dengan %.2f KGs\n", lbs, kgs);
+  }
+  else {
+    printf("masukkan input yang benar! pilih 1 atau 2\n");
+  }
 
   return 0;
 }
