b7322da9a321cc2917c68b2f6eeef3dffb638a21
novariann89-yn <novariann89@gmail.com>
2026-04-10 20:44:26 +0700

    cuma latihan bikin switch lgi, dikit, tapi tetap progress.

--- latihan1.c
+++ latihan1.c
@@ -2,46 +2,23 @@
 
 int main(){
 
-  int angka;
+  int nom;
 
-// ini else if biasa
+  printf("pilih nonor antara 1-3: ");
+  scanf("%d", &nom);
 
-  printf("pilih angka antara 1 - 3 : ");
-  scanf("%d", &angka);
-
-  if (angka == 1){
-    printf("lu pilih satu, dh gt aj hehe\n");
-  }
-  else if (angka == 2){
-    printf("lu pilih angka 2, dh gt aja hehe\n");
-  }
-  else if (angka == 3){
-    printf("lu pilih angka 3, dh gt aja hehe\n");
-  }
-  else {
-    printf("masukin angka yg bener dongok lu\n");
-  }
-
-  // nah kalo yang ini nested if
-
-  char huruf;
-  printf("pilih hurus a, b, atau c : ");
-  scanf("%c", &huruf);
-
-  if (huruf == 'a'){
-    printf("lu pilih huruf a disini\n");
-    if (huruf == 'b'){
-      printf("nah lu pilih b, jadi ngelewatin a\n");
-      if (huruf == 'c'){
-        printf("nah terakhir lu pilih c, berarti ngelewatin a sama b\n");
-      } else {
-        printf("lu ga pilih c\n");
-      }
-    } else {
-      printf("lu ga pilih b\n");
-    } 
-  } else {
-    printf("lu ga pilih a\n");
+  switch(nom){
+    case 1:
+    printf("lu pilih angka 1, dh gt aja\n");
+    break;
+    case 2:
+    printf("lu pilih angka 2, dh gt aja\n");
+    break;
+    case 3:
+    printf("lu pilih angka 3, dh gt aja\n");
+    break;
+    default:
+    printf("masukkan input yang bener!\n");
   }
 
   return 0;
