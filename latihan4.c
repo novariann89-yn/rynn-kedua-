af844a876cedf680e3fba0d776892a2e724e3854
novariann89-yn <novariann89@gmail.com>
2026-04-13 21:33:19 +0700

    yesterday's little code, today is nothing

--- latihan4.c
+++ latihan4.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+  
+  int num1;
+  int num2;
+  char op[5];
 
-  // mencoba membuat tiny program pakai switch statement
-  int nomor;
+  printf("masukkan angka pertama: ");
+  scanf("%d", &num1);
 
-  printf("masukkan nomor antara 1-7 untuk hari : ");
-  scanf("%d", &nomor);
+  printf("masukkan angka kedua: ");
+  scanf("%d", &num2);
 
-  switch(nomor){
-    case 1:
-    printf("hari senin, malas banar\n");
-    break;
-    case 2:
-    printf("hari selasa, masi cukup malas sih\n");
-    break;
-    case 3:
-    printf("hari rabu, oh aja\n");
-    break;
-    case 4:
-    printf("hari kamis, lumayan laa\n");
-    break;
-    case 5:
-    printf("hari jumat, okeee let's go\n");
-    break;
-    case 6:
-    printf("hari sabtu, uuuuyyeaaaaaahhh easy dayy\n");
-    break;
-    case 7:
-    printf("hari minggu, enjoy.\n");
-    break;
-    default:
-    printf("masukkan input dengan tepat!! pilih antara 1-7\n");
-  }
+  printf("masukkan jenis operasi (+, -, *, /): ");
+  scanf("%c", &op);
+
+  
+
   return 0;
 }
