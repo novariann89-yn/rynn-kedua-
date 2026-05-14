59f85d419bdb3cbc66642aeaf35fc72045260cfa
novariann89-yn <237447552+novariann89-yn@users.noreply.github.com>
2026-05-14 22:18:23 +0700

    tonight i re-learned boolean with if else and making weight conversion program

--- latihan2.c
+++ latihan2.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
-#include <string.h>
 #include <math.h>
 
 int main(){
 
-  int spasi=40;
-  char a[50]="jenis jenis format specifier\n \n";
-  printf("%*s",spasi, a);
+  // membuat membuqt program pengkonversi berat
 
-  printf("   dengan penjelasan simpel saja\n"
-         "1. 'persen'd adalah untuk integer data type atau angka bulat\n"
-         "2. 'persen'f adalah untuk float atau angka desimal atau pecahan\n"
-         "3. 'persen'lf adalah double untuk desimal juga but dengan memori lebih besar\n"
-         "4. 'persen'c adalah untuk charachter yaitu berisi satu karakter\n"
-         "5. 'persen's adalah untuk string or charachter dengan ukuran\n \n");
+  float kgs=0.0;
+  float lbs=0.0;
+  int mana=0;
 
-  char arit[40]="Arithmetic Operations\n";
-  printf("%40s\n", arit);
-  printf("dimana ini menggunakan header file <math.h>\n"
-         "aku tidak perlu menghafalnya karena belum menemukan\n"
-         "real use case untuk itu, aku akan belajar jika menemukan\n \n \n");
+  char a[50]="↓↓↓ PROGRAM PENGKONVERSI BEBAN ↓↓↓\n";
+  printf("%60s", a);
 
-  char userin [50]="User input\n \n";
-  printf("%30s", userin);
+  printf("1. kgs ke lbs\n"
+         "2. lbs ke kgs\n \n"
+         "masukkan 1 atau 2 untuk memilih operasi yang mana!.. ");
+  scanf("%d", &mana);
 
-  char nem[10];
-  char fullnem[30];
-  printf("masukkan nama panggilan kamu!.. ");
-  scanf("%s", nem);
+  if (mana ==1){
+    printf("masukkan berat dalam satuan kg.. ");
+    scanf("%f", &kgs);
+    printf("\n");
+    lbs=kgs*2.205;
+    printf("%3f kilogram sama dengan %3f pounds\n", kgs, lbs);
+  } else if (mana ==2){
+    printf("masukkan berat dalam satuan lbs.. ");
+    scanf("%f", &lbs);
+    printf("\n");
+    kgs=lbs/2.205;
+    printf("%3f pounds sama dengan %3f kilogram\n", lbs, kgs);
+  } else {
+    printf("UNKNOWN INPUT!!!\n"
+           "*tolong pilih antara 1 atau 2!\n");
+  }
 
-  printf("masukkan nama panjang kamu!.. ");
-  (void)getchar();
-  fgets(fullnem, sizeof(fullnem), stdin);
-  fullnem[strcspn(fullnem, "\n")]=0;
-  printf("\n");
-
-  printf("halooo perkenalkan nama aku %s\n", nem );
-  printf("perkenalkan, saya %s\n", fullnem);
-
-
-return 0;
+  return 0;
 }
