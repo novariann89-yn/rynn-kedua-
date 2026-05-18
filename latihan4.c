72ce730d8c98dcf331b73f343d360a19a88aa261
novariann89-yn <237447552+novariann89-yn@users.noreply.github.com>
2026-05-18 21:05:50 +0700

    been studied new subject, it is Function.

--- latihan4.c
+++ latihan4.c
@@ -1,58 +1,31 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
+#include <string.h>
+
+void Words(char nama[], int umur){
+printf("saya suka belaiar\n");
+printf("saya hobi belajar\n");
+printf("belajar membuat saya merasa baik\n");
+printf("nama saya %s\n", nama);
+printf("dan umur saya %d tahun\n \n", umur);
+}
 
 int main(){
 
-  bool isCal;
-  int pilih;
+  char nama[30];
+  int umur;
 
-  printf("ketik 1 untuk calculator dan 2 untuk yang lain.. ");
-  scanf("%d", &pilih);
+  printf("siapa nama anda? ");
+  fgets(nama, sizeof(nama), stdin);
+  nama[strcspn(nama, "\n")]=0;
 
-  isCal=(pilih == 1);
+  printf("berapa umur anda? ");
+  scanf("%d", &umur);
 
-  if (isCal){
-
-  //simplest interest rate calculator
-
-  char a[50]="↓↓↓ Interest Rate Calculator ↓↓↓\n";
-  double principal=0.0;
-  double rate=0.0;
-  int years=0;
-  int TimesCompounded=0;
-  double total=0.0;
-
-  printf("%50s", a);
-
-  printf("masukkan berapa yang anda investasikan dalam rupiah.. ");
-  scanf("%lf", &principal);
-
-  printf("masukkan suku bunga nya.. ");
-  scanf("%lf", &rate);
-  rate = rate/100;
-
-  printf("masukkan berapa tahun anda akan berinvestasi.. ");
-  scanf("%d", &years);
-
-  printf("masukkan berapa kali dilipatgandakan.. ");
-  scanf("%d", &TimesCompounded);
-
-  total=principal*pow(1+rate/TimesCompounded,TimesCompounded*years);
-
-  printf("\n");
-  printf("jika yang anda investasikan berjumlah %.3lf\n", principal);
-  printf("dengan bunga %g persen\n", rate);
-  printf("maka hasilnya adalah %.3lf rupiah\n", total);
-  } else {
-    printf("trus mau apa mek, disini cuma bisa calcularor\n");
-  }
-
-
-  printf("\n");
-  char b[50]="↓↓↓ ANOTHER IF ELSE ↓↓↓\n";
-  printf("%30s", b);
-  
+  Words(nama, umur);
+  Words(nama, umur);
+  Words(nama, umur);
+  Words(nama, umur);
+  Words(nama, umur);
 
 
 
