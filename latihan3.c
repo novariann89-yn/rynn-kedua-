804fc161b6f4de98538a858d37c2b90fed56eba8
novariann89-yn <237447552+novariann89-yn@users.noreply.github.com>
2026-05-24 22:52:47 +0700

    learned new subject, for loops

--- latihan3.c
+++ latihan3.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
+void Jeda(int wak){
+  usleep(wak*10000);
+}
 
 int main(){
-  int pilihan;
-  while (pilihan != 3){
-    printf("\n--- program cash app ---\n");
-    printf("1. cek saldo\n");
-    printf("2. transfer\n");
-    printf("3. keluar\n");
-    printf("silahkan masukkan pilihan anda = ");
-    scanf("%d", &pilihan);
-
-   if (pilihan == 1){
-      printf("\nsaldo anda = 4.500.000 rp\n");
-    } else if (pilihan == 2){
-      printf("\nTRANSFER BERHASIL!\n");
-    } else if (pilihan != 3){
-      printf("pilihan invalid! pilih diantara ketiga nomor\n");
-    }
-  } printf("\nterimakasih telah menggunakan program ini!!\n");
+  for(int i=1; i <= 10; i++){
+    Jeda(50);
+    printf("%d\n", i);
+  }
 
   return 0;
 }
